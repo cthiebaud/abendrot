d8145105cad16ec001c6a0ad1fbe7755e63ffff6
Christophe Thiebaud <christophe.thiebaud@sap.com>
2012-11-06 17:48:38 +0100

    added Github Ribbon
    Change-Id: I1f8eaa55c817ef533f2e6596aef21694ba96ac75

---
+++ src/main/java/net/aequologica/cloud/GitRepositoryState.java
@@ -0,0 +1,100 @@
+package net.aequologica.cloud;
+
+import java.util.Properties;
+
+public class GitRepositoryState {
+
+	private final String branch; // =${git.branch}
+	private final String describe; // =${git.commit.id.describe}
+	private final String commitId; // =${git.commit.id}
+	private final String commitIdAbbrev; // =${git.commit.id.abbrev}
+	private final String buildUserName; // =${git.build.user.name}
+	private final String buildUserEmail; // =${git.build.user.email}
+	private final String buildTime; // =${git.build.time}
+	private final String commitUserName; // =${git.commit.user.name}
+	private final String commitUserEmail; // =${git.commit.user.email}
+	private final String commitMessageFull; // =${git.commit.message.full}
+	private final String commitMessageShort; // =${git.commit.message.short}
+	private final String commitTime; // =${git.commit.time}
+
+	public GitRepositoryState(Properties properties) {
+		this.branch = properties.get("git.branch").toString();
+		this.describe = properties.get("git.commit.id.describe").toString();
+		this.commitId = properties.get("git.commit.id").toString();
+		this.commitIdAbbrev = properties.get("git.commit.id.abbrev").toString();
+		this.buildUserName = properties.get("git.build.user.name").toString();
+		this.buildUserEmail = properties.get("git.build.user.email").toString();
+		this.buildTime = properties.get("git.build.time").toString();
+		this.commitUserName = properties.get("git.commit.user.name").toString();
+		this.commitUserEmail = properties.get("git.commit.user.email").toString();
+		this.commitMessageShort = properties.get("git.commit.message.short").toString();
+		this.commitMessageFull = properties.get("git.commit.message.full").toString();
+		this.commitTime = properties.get("git.commit.time").toString();
+	}
+
+	@Override
+	public String toString() {
+		return "GitRepositoryState [" +
+				 "\n\tbranch=" + branch +
+				 "\n\tdescribe=" + describe +
+				 "\n\tcommitId=" + commitId +
+				 "\n\tcommitIdAbbrev=" + commitIdAbbrev +
+				 "\n\tbuildUserName=" + buildUserName +
+				 "\n\tbuildUserEmail=" + buildUserEmail +
+				 "\n\tbuildTime=" + buildTime+
+				 "\n\tcommitUserName=" + commitUserName +
+				 "\n\tcommitUserEmail=" + commitUserEmail +
+				 "\n\tcommitMessageFull=" + commitMessageFull +
+				 "\n\tcommitMessageShort=" + commitMessageShort +
+				 "\n\tcommitTime=" + commitTime + 
+				"\n]";
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public String getDescribe() {
+		return describe;
+	}
+
+	public String getCommitId() {
+		return commitId;
+	}
+
+	public String getCommitIdAbbrev() {
+		return commitIdAbbrev;
+	}
+
+	public String getBuildUserName() {
+		return buildUserName;
+	}
+
+	public String getBuildUserEmail() {
+		return buildUserEmail;
+	}
+
+	public String getBuildTime() {
+		return buildTime;
+	}
+
+	public String getCommitUserName() {
+		return commitUserName;
+	}
+
+	public String getCommitUserEmail() {
+		return commitUserEmail;
+	}
+
+	public String getCommitMessageFull() {
+		return commitMessageFull;
+	}
+
+	public String getCommitMessageShort() {
+		return commitMessageShort;
+	}
+
+	public String getCommitTime() {
+		return commitTime;
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,30 @@
+<%@page import="net.aequologica.cloud.GitRepositoryState"%>
+<%@page import="java.util.Properties"%>
+<%@page import="java.io.IOException"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Hello, SAP NetWeaver Cloud!</title>
+<!-- 
+<%
+Properties properties = new Properties();
+properties.load(getClass().getClassLoader().getResourceAsStream("git.properties"));
+
+GitRepositoryState gitRepositoryState = new GitRepositoryState(properties);
+
+out.write(gitRepositoryState.toString());
+%>
+-->
+</head>
+<body>
+	<h1>Hello, <a href="http://scn.sap.com/community/developer-center/cloud-platform">SAP NetWeaver Cloud</a>!</h1>
+	<a href="https://github.com/cthiebaud/HelloCloud/commit/<%=gitRepositoryState.getCommitId()%>">
+		<img style="position: absolute; top: 0; right: 0; border: 0;"
+			src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"
+			alt="Fork me on GitHub">
+	</a>
+</body>
+</html>
